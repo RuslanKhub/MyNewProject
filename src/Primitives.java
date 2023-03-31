0946b9bea179533f37a1d44eb05930cccd36bff4
Ruslan Khubiev <r.hubiev@tg.dunice.net>
2023-03-31 09:49:50 +0300

    Added all primitives and max min values on new class

---
+++ src/Primitives.java
@@ -0,0 +1,17 @@
+public class Primitives {
+    public static void primitives(){
+        char character = 'H';
+        System.out.println(character);
+        System.out.println(true + "," + false);
+        System.out.println(Byte.MAX_VALUE + ", " + Byte.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE + ", " + Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE + ", " + Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE + ", " + Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE + ", " + Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE + ", " + Double.MIN_VALUE);
+        String str = "this is String message";
+        System.out.println(str);
+        char [] array = {'H','e','l','l','o'};
+        System.out.println(array);
+    }
+}
