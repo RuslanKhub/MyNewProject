b838338dee0abdd072e514b7f2d95d37ab02356f
Ruslan Khubiev <r.hubiev@tg.dunice.net>
2023-03-31 09:31:25 +0300

    Added all primitives and max min values

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char character = 'H';
+        System.out.println(character);
+        System.out.println(true + "," + false);
+        System.out.println(Byte.MAX_VALUE + ", " + Byte.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE + ", " + Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE + ", " + Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE + ", " + Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE + ", " + Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE + ", " + Double.MIN_VALUE);
+        String str = "this is String message";
+        System.out.println(str);
+        char [] array = {'H','e','l','l','o'};
+        System.out.println(array);
     }
 }
